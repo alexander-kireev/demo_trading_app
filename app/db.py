2611f5131c987fd769345e7a0985216a5058f646
alexk <kireev.alexander1997@gmail.com>
2025-09-14 08:22:52 +0100

    finished implementing buy and sell trading_service functionality, cleaned up codebase

--- app/db.py
+++ app/db.py
@@ -18,7 +18,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 class DB:
-    """ class handles all database operations related to users: creation, authentication, updates,
+    """ class handles all database operations : creation, authentication, updates,
         deletion, querying """
     
 ###   --------------------   USER QUERIES START   --------------------   ###
@@ -26,22 +26,21 @@ class DB:
     def establish_connection(self):
         """ returns a psycopg2 db connection """
         
-        conn = psycopg2.connect(
+        return psycopg2.connect(
                 host=DB_HOST,
                 port=DB_PORT,
                 database=DB_NAME,
                 user=DB_USER,
                 password=DB_PASSWORD
             )
-        conn.autocommit = False
-        return conn
-    
-    
+
+       
     def add_user(self, user):
-        # default balance set for new users. adjust as needed.
-        default_balance = 10000.00
 
         """ accepts a user object, inserts it into users table """
+
+        # default balance set for new users. adjust as needed.
+        default_balance = 10000.00
 
         try:
             with self.establish_connection() as conn:
@@ -53,8 +52,8 @@ class DB:
                         user.first_name, user.last_name, user.dob, user.email, user.password_hash, default_balance
                     ))
 
-                conn.commit()
-                return True
+                    conn.commit()
+                    return True
 
         except Exception as e:
             print(f"Error. Failed to insert user: {e}.")
@@ -62,6 +61,7 @@ class DB:
 
 
     def get_user_by_email(self, email):
+
         """ accepts a user's email and returns a user object """
         
         try:
@@ -87,6 +87,7 @@ class DB:
 
 
     def get_user_by_id(self, id):
+
         """ accepts a user's id and returns a user object """
         
         try:
@@ -112,6 +113,7 @@ class DB:
 
 
     def delete_user(self, id):
+
         """ accepts a user_id and removes a user from the users table """
         
         try:
@@ -132,6 +134,7 @@ class DB:
 
     
     def get_user_password_hash(self, id):
+
         """ accepts a user id and returns the user's password_hash from db """
         
         try:
@@ -155,6 +158,7 @@ class DB:
         
 
     def update_user_balance(self, id, amount):
+
         """ accepts a user's id and amount, updating the user's current balance """
         
         try:
@@ -164,9 +168,10 @@ class DB:
                     # get user object and balance, update balance
                     user = self.get_user_by_id(id)
                     new_balance = float(user.balance) + amount
-                    cur.execute(""" UPDATE users SET balance=%s WHERE id=%s""", (new_balance, user.id))
 
+                    cur.execute(""" UPDATE users SET balance=%s WHERE id=%s""", (new_balance, user.id))
                     conn.commit()
+
                     return True
 
         except Exception as e:
@@ -175,13 +180,13 @@ class DB:
 
 
     def check_email_exists(self, email):
+
         """ accepts an email and returns true if email is already in use """
         
         try:
             with self.establish_connection() as conn:
                 with conn.cursor() as cur:
                     cur.execute(""" SELECT email FROM users WHERE email=%s""", (email,))
-
                     row = cur.fetchone()
 
                     if row:
@@ -195,6 +200,7 @@ class DB:
 
 
     def authenticate_user(self, email, password):
+
         """ accepts user's email and password, returns user id if login is successful """
         
         try:
@@ -204,7 +210,6 @@ class DB:
                     # ensure email is register to an account
                     if self.check_email_exists(email):
                         cur.execute("""SELECT id, password_hash FROM users WHERE email=%s""", (email,))
-
                         row = cur.fetchone()
 
                         # ensure query returned valid result
@@ -223,12 +228,14 @@ class DB:
                     else:
                         print("Email is not registered to an account.")
                         return None
+                    
         except Exception as e:
             print(f"Error. Could not authenticate user: {e}.")
             return None
 
     
     def update_user_email(self, id, new_email, password):
+
         """ accepts id, new email and current password, updates user's email in db """
         
         try:
@@ -250,12 +257,14 @@ class DB:
                     else:
                         print("Email is already in use.")
                         return True
+                    
         except Exception as e:
             print(f"Error. Unable to update user email: {e}")
             return False
 
         
     def update_user_password(self, id, current_password, new_password):
+
         """ accepts id, new password and current password, updates user's password in db """
         
         try:
@@ -269,8 +278,8 @@ class DB:
                         # hash new password
                         new_hashed_password = bcrypt.hashpw(new_password.encode(), bcrypt.gensalt()).decode()
                         cur.execute(""" UPDATE users SET password_hash=%s WHERE id=%s""", (new_hashed_password, id))
-
                         conn.commit()
+
                         return True
                     else:
                         print("Incorrect password.")
@@ -292,6 +301,8 @@ class DB:
         try:
             with self.establish_connection() as conn:
                 with conn.cursor() as cur:
+
+                    # log the trade
                     cur.execute("""
                         INSERT INTO trades_log (user_id, company_name, symbol, price_per_share, 
                                                 number_of_shares, transaction_total, transaction_type)
@@ -302,52 +313,17 @@ class DB:
                         trade.number_of_shares, trade.transaction_total, trade.transaction_type
                     ))
 
+                    # get id of traded logged, return it
+                    # this is done to ensure trade can be removed from from log later
+                    # if other issues arise and trade falls through
                     (trade_id,) = cur.fetchone()
                     conn.commit()
+
                     return trade_id
-                    
-                    # THIS SEEMS REDUNDANT, AS RETURNING FROM QUERY WORKED?
-                    cur.execute("""
-                            SELECT trade_id FROM trades_log
-                            WHERE user_id = %s AND symbol = %s AND price_per_share = %s
-                            AND number_of_shares = %s AND transaction_total = %s AND transaction_type = %s
-                            ORDER BY trade_id DESC
-                            LIMIT 1
-                        """, (
-                            trade.user_id, trade.symbol, trade.price_per_share,
-                            trade.number_of_shares, trade.transaction_total, trade.transaction_type
-                    ))
-                    
-                    
+                                    
         except Exception as e:
             print(f"Error. Failed to log trade: {e}.")
             return None  
-
-    # THIS SEEMS REDUNDANT, AS RETURNING FROM QUERY WORKED?
-    def get_last_trade_id(self, trade):
-        try:
-            with self.establish_connection() as conn:
-                with conn.cursor() as cur:
-                    cur.execute("""
-                            SELECT trade_id FROM trades_log
-                            WHERE user_id = %s AND symbol = %s AND price_per_share = %s
-                            AND number_of_shares = %s AND transaction_total = %s AND transaction_type = %s
-                            ORDER BY trade_id DESC
-                            LIMIT 1
-                        """, (
-                            trade.user_id, trade.symbol, trade.price_per_share,
-                            trade.number_of_shares, trade.transaction_total, trade.transaction_type
-                    ))
-                    result = cur.fetchone()
-                    print(result)
-                    if result:
-                        return (result[0],)
-                    else:
-                        raise Exception
-        except Exception as e:
-            print(f"Error. Failed to retrieve last trade_id: {e}.")
-            return None
-
 
 
     def remove_trade(self, trade_id):
@@ -358,10 +334,9 @@ class DB:
             with self.establish_connection() as conn:
                 with conn.cursor() as cur:
                     cur.execute(""" DELETE FROM trades_log WHERE trade_id=%s """, (trade_id,))
-
-                    
-                    
+                   
                     return True
+                
         except Exception as e:
             print(f"Error. Failed to remove trade from database: {e}.")
             return False
@@ -376,7 +351,6 @@ class DB:
                 with conn.cursor() as cur:
                     cur.execute("""SELECT * FROM all_holdings WHERE user_id=%s AND symbol=%s""",
                                 (user_id, symbol))
-                    
                     row = cur.fetchone()
 
                     # check if holding was found
@@ -393,11 +367,19 @@ class DB:
             print(f"Error. Failed to retrieve single stock holding: {e}.")
             return None
 
-
+    # current_holding is only passed into the method IF user already
+    # has shares of a particular company
     def update_user_single_holding(self, user_id, trade, current_holding=None):
+
+        """ accepts user's id, trade object, OPTIONAL current_holding object,
+            if user already has shares of particular company, updates the 
+            all_holdings table to reflect change in user's holdings """
+
         try:    
             with self.establish_connection() as conn:
                 with conn.cursor() as cur:
+
+                    # if user already has shares of particular company
                     if current_holding:
                         
                         # check type of trade (BUY OR SELL) 
@@ -408,16 +390,21 @@ class DB:
                         else:
                             raise Exception
                         
-                        
+                        # remove holding if user has 0 shares of it left
                         if updated_number_of_shares == 0:
                             cur.execute("""
                                         DELETE FROM all_holdings WHERE user_id=%s AND symbol=%s
                                         """,
                                         (user_id, trade.symbol))
+                        
+                        # else, update number of shares held
                         else:
-                            updated_price_per_share = (trade.transaction_total + 
-                                                    (current_holding.number_of_shares * current_holding.price)) / updated_number_of_shares
+
+                            # get new average price per share
+                            updated_price_per_share = ((current_holding.number_of_shares * current_holding.price) 
+                                                       - trade.transaction_total) / updated_number_of_shares
                             
+                            # update holding 
                             updated_holding = Holding(Stock(company_name=trade.company_name, symbol=trade.symbol,
                                                     price=updated_price_per_share), number_of_shares=updated_number_of_shares)
 
@@ -428,11 +415,10 @@ class DB:
                                         (updated_holding.number_of_shares, updated_holding.price, updated_holding.total_value, user_id)
                             )
                                  
-
+                    # if user currently has 0 shares of particular company
                     else:
                         holding = Holding(Stock(company_name=trade.company_name, symbol=trade.symbol,
                                                 price=trade.price_per_share), number_of_shares=trade.number_of_shares)
-                        
                         
                         cur.execute(""" 
                                     INSERT INTO all_holdings (user_id, company_name, symbol, 
@@ -467,10 +453,6 @@ class DB:
         # except Exception as e:
         #     print("Error. : {e}.")
         #     return False
-
-
-
-
 
 
 if __name__ == "__main":
--- app/trading_service_model.py
+++ app/trading_service_model.py
@@ -16,6 +16,7 @@ class TradingService:
 
         if user.balance < transaction_amount:
             print("Insufficient funds to purchase requested shares.")
+            return False
         else:
 
             try:
@@ -29,54 +30,72 @@ class TradingService:
                 if not db.update_user_single_holding(user_id=user_id, trade=trade, current_holding=current_holding):
                     print("in wrong loop!")
                     db.remove_trade(trade_id)
+                    raise Exception
                 
                 # update user's balance to reflect purchase
                 db.update_user_balance(user_id, (transaction_amount * -1 ))
+
+                return True
       
             except Exception as e:
                 print(f"Error. Failed to purchase shares: {e}.")
                 
-    # TODO: MUST COMPLETE, SIMILAR TO BUY_STOCK()
+    # DOES NOT ALLOW FOR SHORT SELLING
     def sell_stock(self, stock, num_shares, user_id):
 
-        """ accepts stock object, number of shares to buy, user id and buys the stock """
+        """ accepts stock object, number of shares to sell, user id and sells the stock.
+            DOES NOT ALLOW FOR SHORT SELLING """
 
-        # get user_balance
         db = DB()
-        user = db.get_user_by_id(user_id)
 
         try:
+
+            # get user's current holding of this company
             current_holding = db.get_user_single_holding(user_id=user_id, symbol=stock.symbol)
 
+            # if user DOES NOT have enough shares to sell
             if current_holding.number_of_shares < num_shares:
                 print("Insufficient shares held to sell.")
+                return False
+            
+            # if user DOES have enough shares to sell
             else:
+
+                # instantiate trade object, log the trade
                 trade = Trade(user_id=user_id, stock=stock, number_of_shares=num_shares, transaction_type="SELL")
                 trade_id = db.log_trade(trade)
 
+                # update the user's holding to reflect change. if the update fails, remove the trade from the log
                 if not db.update_user_single_holding(user_id=user_id, trade=trade, current_holding=current_holding):
                     db.remove_trade(trade_id)
-
+                    raise Exception
+                
+                # update user's balance to reflect sale
+                db.update_user_balance(user_id, trade.transaction_total)
+                
+                return True
+                
         except Exception as e:
-            print(f"Error. Failed to purchase shares: {e}.")
+            print(f"Error. Failed to liquidate shares: {e}.")
 
 
 
 if __name__ == "__main__":
 
+    pass
 
-    # init stock
-    company_name = "apple"
-    symbol = "aapl"
-    price = 125.8
-    appl_stock = Stock(company_name, symbol, price)
+    ## init stock
+    # company_name = "apple"
+    # symbol = "aapl"
+    # price = 125.8
+    # appl_stock = Stock(company_name, symbol, price)
 
-    # init trade
-    user_id = 1
-    number_of_shares = 3
-    transaction_type = "BUY"
+    # # init trade
+    # user_id = 1
+    # number_of_shares = 1
+    # transaction_type = "SELL"
 
-    ts = TradingService()
-    ts.buy_stock(stock=appl_stock, num_shares=3, user_id=user_id)
-    # ts.sell_stock(stock=appl_stock, num_shares=3, user_id=user_id)
+    # ts = TradingService()
+    # ts.sell_stock(stock=appl_stock, num_shares=number_of_shares, user_id=user_id)
+    # # ts.sell_stock(stock=appl_stock, num_shares=3, user_id=user_id)
 
