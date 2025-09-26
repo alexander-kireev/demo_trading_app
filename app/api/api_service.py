815beb48356aa2515783f0e38349ff04b11412ab
Alexander Kireev <kireev.alexander1997@gmail.com>
2025-09-26 10:25:09 +0100

    implemented API fetching functionality for stock object

---
+++ app/api/api_service.py
@@ -0,0 +1,24 @@
+from yfinance import Ticker, Tickers
+
+from app.stock.stock_model import Stock
+
+
+# tested, functional, commented
+def create_stock(symbol):
+    """ Accepts a stock symbol, fetches live stock price and returns stock object. """
+    
+    info = Ticker(symbol).info
+    return Stock(info["shortName"], symbol, info["regularMarketPrice"])
+    
+
+# tested, functional, commented
+def create_stocks(symbols):
+    """ Accepts a list of stock symbols, returns a dictionary with stock symbols as keys and stock objects
+        as values. """
+    
+    stocks = {}
+
+    for symbol in symbols:
+        stocks[symbol] = create_stock(symbol)
+    
+    return stocks
